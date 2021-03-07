be25007d07240fd5355d904a93f646e6bd31546c
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2021-03-07 13:02:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-	res.send('Hello World! Cloud Foundry Node JS Demo');
+	res.send('Hello World! Heroku Node JS Demo');
 });
 
 const port = process.env.PORT || 5000;
