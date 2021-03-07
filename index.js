6895a008580aa59146dafb315a51280959f03882
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2021-03-07 13:41:12 +0530

    Added HackerRank Badges API

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 
+app.get('/hackerrank_badges', function (req, res) {
+	fetch('https://www.hackerrank.com/rest/hackers/UtkarshPathrabe/badges')
+		.then((response) => response.json())
+		.then((json) => res.send(json))
+		.catch((err) => {
+			console.log('Error in hackerrank_badges. Details: ', err);
+			res.send('Error');
+		});
+});
+
 app.get('/', function (req, res) {
-	res.send('Hello World! Heroku Node JS Demo');
+	res.send(
+		'Hello World! Heroku Node JS application for my Github.io Page https://utkarshpathrabe.github.io/',
+	);
+});
+
+app.get('*', function (req, res) {
+	res.send('Invalid URL');
 });
 
 const port = process.env.PORT || 5000;
